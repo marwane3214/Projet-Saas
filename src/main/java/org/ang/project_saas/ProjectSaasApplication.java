c93ed766b871fc76eb7ecb18d2fbb608637d3fd2
ZEO <znafil177@gmail.com>
2025-10-21 00:30:38 +0100

    second commit

---
+++ src/main/java/org/ang/project_saas/ProjectSaasApplication.java
@@ -0,0 +1,13 @@
+package org.ang.project_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ang/project_saas/ProjectSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ang.project_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
